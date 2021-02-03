9b7267306a9588635df5ed2cb1daefb0a8f5588e
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-02-03 18:00:11 +0000

    Movement and Jumping done

--- FMP_Game/Assets/PlayerAnimations.cs
+++ FMP_Game/Assets/PlayerAnimations.cs
@@ -6,17 +6,87 @@ public class PlayerAnimations : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator m_Anim;
+    Rigidbody2D m_rb;
+
+    bool isFacingLeft;
 
     void Start()
     {
         m_Anim = GetComponent<Animator>();
+        m_rb = GetComponent<Rigidbody2D>();
+        m_rb.velocity = Vector2.zero;
+        isFacingLeft = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Is he moving?
+        //if (m_rb.velocity.x != 0 || m_rb.velocity.y != 0)
+        //{
+        //    m_Anim.SetTrigger("isMoving");
+        //    print("MOVE");
+        //}
+        //else
+        //{
+        //    m_Anim.SetTrigger("isIdle");
+        //    print("IDLE");
+        //}
+
+        //if (m_rb.velocity.x < 0.1 && m_rb.velocity.x > -0.1)
+        //{
+        //    m_Anim.SetTrigger("isIdle");
+        //    print("IDLE");
+        //}
+        //else
+        //{
+        //    m_Anim.SetTrigger("isMoving");
+        //    print("MOVE");
+        //}
+
+        //Is he moving to the left
+        if (m_rb.velocity.x < 0)
+        {
+            //is he already facing left?
+            if (!isFacingLeft)
+            {
+                FlipSprite();
+                isFacingLeft = true;
+            }
+        }
+        else if (m_rb.velocity.x > 0)
+        {
+            if (isFacingLeft)
+            {
+                FlipSprite();
+                isFacingLeft = false;
+            }
+        }
+        print(m_rb.velocity.x);
     }
 
+    private void FlipSprite()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
 
+    public void SetGun(bool hasGun)
+    {
+        m_Anim.SetBool("hasGun", hasGun);
+    }
+
+    public void SetJumpAnim()
+    {
+        m_Anim.SetTrigger("isJumping");
+    }
+
+    public void SetMove()
+    {
+        m_Anim.SetTrigger("isMoving");
+    }
+
+    public void SetIdle()
+    {
+        m_Anim.SetTrigger("isIdle");
+    }
 }
--- FMP_Game/Assets/PlayerMovement.cs
+++ FMP_Game/Assets/PlayerMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D m_rb;
+    Vector2 left;
+    Vector2 right;
+    PlayerAnimations p_Anim;
+
+    public float speed;
+    public int jumpHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rb = GetComponent<Rigidbody2D>();
+        p_Anim = GetComponent<PlayerAnimations>();
+
+        speed = 1.25f;
+        jumpHeight = 150;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        else if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            p_Anim.SetIdle();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void MoveLeft()
+    {
+        m_rb.velocity = new Vector2(-speed, m_rb.velocity.y);
+        p_Anim.SetMove();
+    }
+
+    private void MoveRight()
+    {
+        // m_rb.AddForce(new Vector2(1, m_rb.velocity.y));
+        m_rb.velocity = new Vector2(speed, m_rb.velocity.y);
+        p_Anim.SetMove();
+    }
+
+    private void Jump()
+    {
+        m_rb.AddForce(new Vector2(0, jumpHeight));
+        p_Anim.SetJumpAnim();
     }
 }
