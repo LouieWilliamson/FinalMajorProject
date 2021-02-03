4c44b0f7750e0a47f86db20c7a64eed1d99c6d6e
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-02-03 16:15:54 +0000

    Imported Test Assets, started player animations and movement

---
+++ FMP_Game/Assets/PlayerAnimations.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator m_Anim;
+
+    void Start()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
