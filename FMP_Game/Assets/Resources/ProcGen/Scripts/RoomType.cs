08acb57c7726b0b77e2a5f77923328fcd2e583d6
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-02-22 19:09:07 +0000

    Spawning Player and Gun in room 1

---
+++ FMP_Game/Assets/Resources/ProcGen/Scripts/RoomType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomType : MonoBehaviour
+{
+    public int type;
+    // 0 -- LR
+    // 1 -- LRB
+    // 2 -- LRT
+    // 3 -- LRTB
+    public void DestroyRoom()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ FMP_Game/Assets/Resources/ProcGen/Scripts/StartingRoom.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingRoom : MonoBehaviour
+{
+    //spawn the player
+    //spawn gun powerup
+    private GameObject playerPrefab;
+    private Cinemachine.CinemachineVirtualCamera cam;
+    private LevelGeneration lvlGenerator;
+    private bool spawnedPlayer;
+
+    private Vector3 gunSpawn;
+    private float gunX;
+    private float gunY;
+    private float gunYoffset;
+    private GameObject gunPowerup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player spawning
+        spawnedPlayer = false;
+        lvlGenerator = GameObject.Find("LevelGenerator").GetComponent<LevelGeneration>();
+        cam = GameObject.FindGameObjectWithTag("Camera").GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        playerPrefab = (GameObject)Resources.Load("Platformer/Prefabs/Player");
+
+        //gun spawning
+        gunPowerup = (GameObject)Resources.Load("Platformer/Prefabs/GunPowerup");
+        gunYoffset = 2.5f;
+        gunX = transform.position.x + Random.Range(-6, 7);
+        gunY = transform.position.y - gunYoffset;
+        gunSpawn = new Vector3(gunX, gunY, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the level is built and the player hasn't been spawned
+        if (lvlGenerator.stopBuilding && !spawnedPlayer)
+        {
+            SpawnPlayer();
+            SpawnGun();
+        }
+    }
+
+    void SpawnPlayer()
+    {
+        spawnedPlayer = true;
+        GameObject player;
+        player = (GameObject)Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        cam.Follow = player.transform;
+
+        //setting the player values for suitable testing in the proc gen world
+        player.transform.localScale = new Vector3(3, 3, 3);
+        player.GetComponent<PlayerMovement>().speed = 5;
+        player.GetComponent<PlayerMovement>().jumpHeight = 500;
+        player.GetComponent<Rigidbody2D>().gravityScale = 1.7f;
+    }
+
+    void SpawnGun()
+    {
+        GameObject gun;
+        gun = (GameObject)Instantiate(gunPowerup, gunSpawn, Quaternion.identity);
+        gun.transform.localScale = new Vector3(3, 3, 3);
+    }
+}
