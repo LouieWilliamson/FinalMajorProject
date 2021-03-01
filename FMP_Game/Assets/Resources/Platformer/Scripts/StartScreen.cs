d4b5aa6514bc3d50576b0d26a2e7bb6f60a0c39f
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-03-01 15:18:05 +0000

    StartScreen and controls done - settings started

---
+++ FMP_Game/Assets/Resources/Platformer/Scripts/StartScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int Level1;
+    public GameObject controls;
+    public GameObject settings;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level-1");
+    }
+    public void OpenControls()
+    {
+        controls.SetActive(true);
+    }
+    public void CloseControls()
+    {
+        controls.SetActive(false);
+    }
+    public void OpenSettings()
+    {
+        settings.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        settings.SetActive(false);
+    }
+    public void QuitGame()
+    {
+
+    }
+}
