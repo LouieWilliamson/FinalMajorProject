bae349d1f1abfcf8ef380877da8642802a98334c
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-03-10 16:16:47 +0000

    simplified parallax script

--- FMP_Game/Assets/Resources/Platformer/Scripts/Parallax.cs
+++ FMP_Game/Assets/Resources/Platformer/Scripts/Parallax.cs
@@ -4,26 +4,12 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject bg;
-    public GameObject fg;
-    private Rigidbody2D fg_rb;
-    private GameObject player;
-    private Rigidbody2D p_rb;
+    public Rigidbody2D fg_rb;
     public float speed;
-
-    public Transform leftBound;
-    public Transform rightBound;
-
-    private bool levelLoaded;
-    private bool playerSaved;
     public Camera cam;
-    void Start()
-    {
-        fg_rb = fg.GetComponent<Rigidbody2D>();
-    }
+
     private void FixedUpdate()
     {
-            fg_rb.velocity = new Vector2(-cam.velocity.x * speed, -cam.velocity.y * speed);
+        fg_rb.velocity = -cam.velocity * speed;
     }
 }
