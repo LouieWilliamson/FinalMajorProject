baf56d57cc37a6d68ef2c7c250a3b51a775ce616
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-03-12 12:33:51 +0000

    Assets - UI, Font and Spikes

---
+++ FMP_Game/Assets/Resources/Platformer/Scripts/WeaponUpgrade.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponUpgrade : MonoBehaviour
+{
+    public Upgrade type;
+    public Sprite[] UpgradeSprites;
+
+    private SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        switch (type)
+        {
+            case Upgrade.Cooldown:
+
+                break;
+            case Upgrade.Laser:
+
+                break;
+            case Upgrade.Grenade:
+
+                break;
+            default:
+                print("Error: No Upgrade Type Selected");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Inventory>().StoreUpgrade(type);
+            Destroy(this.gameObject);
+        }
+    }
+}
