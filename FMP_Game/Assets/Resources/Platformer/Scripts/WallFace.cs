dc741cb80e9534da719e5d1b1337819514cde54c
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-03-17 17:43:12 +0000

    Start Room Drawn

---
+++ FMP_Game/Assets/Resources/Platformer/Scripts/WallFace.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFace : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator FaceAnim;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            print("Move Face In");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            print("Move Face Out");
+        }
+    }
+}
