5707489195479b92281e263202f4819bee648968
LouieWilliamson <60222197+LouieWilliamson@users.noreply.github.com>
2021-04-22 13:41:57 +0100

    End Dialogue Setup

--- FMP_Game/Assets/Resources/Scripts/Gameplay/Dialogue.cs
+++ FMP_Game/Assets/Resources/Scripts/Gameplay/Dialogue.cs
@@ -7,6 +7,9 @@ public class Dialogue
 {
     public string name;
 
+    //[TextArea(3, 10)]
+    //public string[] sentences;
+
     [TextArea(3, 10)]
-    public string[] sentences;
+    public List<string> sentences = new List<string>();
 }
